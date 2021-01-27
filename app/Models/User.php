5705695dd75b24310e8316dff20d0a93114e972b
sean <suiyublg@gmail.com>
2021-01-27 09:43:37 +0000

    初始化代码v2021.01.27.001用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,11 +16,11 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 
     /**
      * The attributes that should be hidden for arrays.
