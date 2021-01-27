f9aa137ab6e0b7c6a883e58db1cc36cbb087d40f
vagrant <vagrant@homestead>
2021-01-27 08:33:34 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
