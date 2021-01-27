2050d6175d74efaffd0cb9430f6ba3a13a02f345
sean <suiyublg@gmail.com>
2021-01-27 10:01:12 +0000

    v2021.01.27.004中文的错误提示信息

--- config/app.php
+++ config/app.php
@@ -133,7 +133,7 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
-
+    'locale' => 'zh_CN',
     'providers' => [
 
         /*
@@ -158,7 +158,7 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
